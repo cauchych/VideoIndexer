3e520afabf87b95498a3956f0583ce2e7b68124d
Elizabeth Deng <elizabethdeng@gmail.com>
2012-04-29 15:38:27 -0700

    back to input stream

--- src/VideoIndexer.java
+++ src/VideoIndexer.java
@@ -74,8 +74,8 @@ public class VideoIndexer {
 
 
 		try{
-			File file = new File("C:/Users/Cauchy/Documents/CSCI576/Project/vdo4/vdo4.rgb"); // TODO change this path to your own
-			audio = new File("C:/Users/Cauchy/Documents/CSCI576/project/vdo4/vdo4.wav"); // TODO change this path to you own
+			File file = new File("C:/Users/edeng/Documents/School/s10/576/project/vdo4/vdo4.rgb"); // TODO change this path to your own
+			audio = new File("C:/Users/edeng/Documents/School/s10/576/project/vdo4/vdo4.wav"); // TODO change this path to you own
 
 			waveStream = new FileInputStream(audio);
 			int audiolen = (int) audio.length();
@@ -187,7 +187,7 @@ public class VideoIndexer {
 		audioInputStream = null;
 		try {
 			InputStream bufferedIn = new BufferedInputStream(waveStream);
-		    audioInputStream = AudioSystem.getAudioInputStream(bufferedIn);
+		    audioInputStream = AudioSystem.getAudioInputStream(waveStream);
 		} catch (UnsupportedAudioFileException e1) {
 			System.out.println(e1);
 		    //throw new PlayWaveException(e1);
@@ -445,7 +445,7 @@ public class VideoIndexer {
 		    audioInputStream = null;
 			try {
 				InputStream bufferedIn = new BufferedInputStream(waveStream);
-				audioInputStream = AudioSystem.getAudioInputStream(bufferedIn);
+				audioInputStream = AudioSystem.getAudioInputStream(waveStream);
 			    //audioInputStream.skip(temp * buffersize);
 			} catch (UnsupportedAudioFileException e1) {
 				System.out.println(e1);
@@ -464,6 +464,8 @@ public class VideoIndexer {
 			// opens the audio channel
 			dataLine = null;
 			try {
+				int offset = temp * 30 * (int) audioFormat.getFrameRate() * audioFormat.getFrameSize() / numFrames;
+				System.out.println("skipping " + offset + " bytes");
 				audioInputStream.skip(temp * 30 * (int) audioFormat.getFrameRate() * audioFormat.getFrameSize() / numFrames);
 			    dataLine = (SourceDataLine) AudioSystem.getLine(info);
 			    dataLine.open(audioFormat, buffersize);
